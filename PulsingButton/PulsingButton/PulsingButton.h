eec97b6d8a4444b043061e42afba89a2734a37f4
Sergey Makeev <makeev.87@gmail.com>
2020-03-21 23:05:09 +0300

    initial project commit

---
+++ PulsingButton/PulsingButton/PulsingButton.h
@@ -0,0 +1,19 @@
+//
+//  PulsingButton.h
+//  PulsingButton
+//
+//  Created by Sergey Makeev on 21.03.2020.
+//  Copyright © 2020 SOME projects. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PulsingButton.
+FOUNDATION_EXPORT double PulsingButtonVersionNumber;
+
+//! Project version string for PulsingButton.
+FOUNDATION_EXPORT const unsigned char PulsingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PulsingButton/PublicHeader.h>
+
+
